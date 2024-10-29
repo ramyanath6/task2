5c79d0b7e52873d7b3cfaca509b40217145e3411
Davineni Ramya <davineniramya@gmail.com>
2024-10-30 00:21:42 +0530

    Add files via upload
    no

--- App.jsx
+++ App.jsx
@@ -1,15 +1,30 @@
-import React from 'react'
+import React, { useState } from 'react'
+import './App.css'
 
-const App = () => {
-  function func1(){
-    alert('hello welcome to the world')
+function App() {
+  var [count, setCount] = useState(0)
+
+  var increment = () => {
+    setCount(count + 1)
   }
+
+  var decrement = () => {
+    if (count > 0) {
+      setCount(count - 1)
+    }
+  }
+
   return (
     <div>
-      <h1 style={{color:'green'}}>Click the below button</h1>
-      <button  style={{color:'brown'}}onClick={func1}>click</button>
+
+      <h1 style={{ color:"purple",background:"pink" }}>Counter App</h1>
+      <h2 style={{ color:"pink",background:"purple" }}>{count}</h2>
+      
+    <button onClick={decrement} style={{ color:"red",background:'green'}}disabled={count === 0}>Decrement</button>   
+    <button onClick={increment} style={{ color:"green",background:'red',marginLeft:'50px'}}>Increment</button>
+
     </div>
   )
 }
 
-export default App
+export default App;
